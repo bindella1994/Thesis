2a483df2c5487a6c24410581670dceabc152857e
bindella1994 <91409341+bindella1994@users.noreply.github.com>
2021-09-27 10:59:38 +0200

    Fix on plotting curve fit

--- Linux_testFeatures/scrubbingTest/plotResult/main.m
+++ Linux_testFeatures/scrubbingTest/plotResult/main.m
@@ -2,26 +2,7 @@ clear all
 clc
 close all
 
-dataTotali = readtable('scrubbingTestCorruzioniTotali.csv');
 
-
-indexTotali = dataTotali.Index;
-percentageTotali = dataTotali.percentage;
-
-
-len = size(dataTotali, 1);
-
-figure(1)
-plot(indexTotali,percentageTotali,'LineWidth',2);
-
-xlabel('Numero di errori per iterazione')
-ylabel('Percentuale di blocchi di dati corrotti[%]')
-legend('Rapporto percentuale tra byte corrotti e byte totali in memoria')
-grid on;
-
-saveas(gcf,'scrubbingTestTotali.png')
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 dataParziali = readtable('scrubbingTestCorruzioniParziali.csv');
 
@@ -32,22 +13,19 @@ percentageParziali = dataParziali.percentage;
 
 len = size(dataParziali, 1);
 
+p = polyfit(indexParziali,percentageParziali,4)
+xForFit = linspace(1,200,200)
+y1 = polyval(p,xForFit)
+
+
 figure(2)
-plot(indexParziali,percentageParziali,'LineWidth',2);
+scatter(indexParziali,percentageParziali,'LineWidth',2);
+hold on;
+plot(xForFit,y1,'LineWidth',2,LineStyle='--');
 
 xlabel('Numero di errori per iterazione')
-ylabel('Percentuale di blocchi di dati corrotti[%]')
-legend('Rapporto percentuale tra byte corrotti e byte totali in memoria')
+ylabel('Rapporto percentuale di byte corrotti  e byte totali in memoria[%]')
+legend('Data','Fitted curve')
 grid on;
 
 saveas(gcf,'scrubbingTestParziali.png')
-
-
-
-p = polyfit(indexParziali,percentageParziali,5)
-y1 = polyval(p,indexParziali);
-figure(3)
-plot(indexParziali,percentageParziali,'o')
-hold on
-plot(indexParziali,y1)
-hold off
